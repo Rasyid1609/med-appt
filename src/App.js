bd72ae538841d3e2668c1e0c95c32cca9fec056e
Rasyid1609 <rasyid.mahardian@gmail.com>
2023-12-24 16:04:10 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Landing_Page from './Components/Landing_Page/Landing_Page';
 import Login from './Components/Login/Login';
 import Sign_Up from './Components/Sign_Up/Sign_Up';
 import InstantConsultation from './Components/InstantConsultation/InstantConsultation';
+import FindDoctorSearch from './Components/FindDoctorSearch/FindDoctorSearch';
+import BookingConsultation from './Components/BookingConsultation';
 
 function App() {
   return (
@@ -16,7 +18,9 @@ function App() {
           <Route path='/' element={<Landing_Page/>} />
           <Route path='/Login' element={<Login/>} />
           <Route path='/Sign_Up' element={<Sign_Up/>} />
-          <Route path='instant-consultation' element={<InstantConsultation/>}/>
+          <Route path='/instant-consultation' element={<InstantConsultation/>}/>
+          <Route path='/finddoctor' element={<FindDoctorSearch />} />
+          <Route path='/search/doctors' element={<BookingConsultation/>} />
         </Routes>
       </BrowserRouter>
     </div>
